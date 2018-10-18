ac91cf174cbae5764cd5307f4a30021944e746ea
Johann Helbling <j.helbling@comparus.de>
2018-10-18 23:16:48 +0200

    Initial commit

--- packet-cli.rb
+++ packet-cli.rb
@@ -1,28 +1,20 @@
-# require "language/go"
+require "language/go"
 
 class PacketCli < Formula
   desc "Official Packet CLI [WIP]"
   homepage "https://packet.net"
   url "https://github.com/packethost/packet-cli/archive/0.0.2.tar.gz"
   sha256 "1312a9b40a8178e6c106efe887e886b46cf4ff90dd80597e7bc7e46059392e8f"
-
-  bottle do
-    cellar :any
-  end
-
   depends_on "go" => :build
 
   def install
-    mkdir_p buildpath/"src/github.com/packethost"
-    ln_s buildpath, buildpath/"src/github.com/packethost/packet-cli"
+    ENV["GOPATH"] = buildpath
+    path = buildpath/"src/github.com/packethost/packet-cli"
+    path.install Dir["*"]
 
-    ENV["GOPATH"] = "#{buildpath}/Godeps/_workspace:#{buildpath}"
-
-    Language::Go.stage_deps resources, buildpath/"src"
-
-    system "go", "build", "-o", "packet-cli"
-
-    bin.install "packet-cli"
+    cd path do
+      system "go", "build", "-o", "#{bin}/packet-cli"
+    end
   end
 
   test do
